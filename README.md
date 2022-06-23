914e1e14f0aa8c670473b1d4785c1284333cc8a6
Александр Фокин <wegnagun@bk.ru>
2022-06-23 16:23:22 +0300

    start working

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # api_final
 api final
+
+### Установка (windows)
+`python -m venv venv `
+
+`venv/Scripts/activate `
+
+`python -m pip install --upgrade pip `
+
+`pip install -r requirements.txt `
+
+### Запуск
+Перейдити в дирректорию yatube_api и выполните миграции:
+
+`python manage.py migrate `
+
+Запустите сервер:
+
+`python manage.py runserver`
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
+router.register(r'follow', GroupViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt'))
 ]
